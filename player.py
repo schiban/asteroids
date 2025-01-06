6e0563b3cad77847fc2e8a5a1b8555d59a1e1eb1
schiban <j_bandeira_@hotmail.com>
2025-01-06 14:20:11 +0000

    Remove spaces from player file

--- player.py
+++ player.py
@@ -7,10 +7,8 @@ class Player(CircleShape):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
 
-    
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
-
 
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -20,11 +18,9 @@ class Player(CircleShape):
         c = self.position - forward * self.radius + right
         return [a, b, c]
 
-
     def rotate(self, dt):
         self.rotation += PLAYER_TURN_SPEED * dt
 
-    
     def update(self, dt):
         keys = pygame.key.get_pressed()
 
@@ -36,7 +32,6 @@ class Player(CircleShape):
             self.move(-dt)
         if keys[pygame.K_d]:
             self.rotate(dt)
-
 
     def move(self, dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
